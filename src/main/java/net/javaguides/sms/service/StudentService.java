bf189aa23213774802d7b3571e45b7eb52ffae36
Umar007 <mail2umarss@gmail.com>
2024-09-27 12:28:01 +0530

    adding core files to github

---
+++ src/main/java/net/javaguides/sms/service/StudentService.java
@@ -0,0 +1,16 @@
+package net.javaguides.sms.service;
+
+import net.javaguides.sms.dto.StudentDto;
+import java.util.List;
+
+public interface StudentService {
+    List<StudentDto> getAllStudents();
+
+    void createStudent(StudentDto student);
+
+    StudentDto getStudentById(Long studnetId);
+
+    void updateStudent(StudentDto studentDto);
+
+    void deleteStudent(Long studentId);
+}
